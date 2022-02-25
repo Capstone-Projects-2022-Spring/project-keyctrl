1f22fe5a6ab9e1f7663f31c7661a287a1b3f1798
ColinHarker <colinharker5@yahoo.com>
2022-02-25 15:50:06 -0500

    Adding client based server connection

--- KeyCtrl/src/components/MultiplayerPage/Multiplayer.js
+++ KeyCtrl/src/components/MultiplayerPage/Multiplayer.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import TextField from "@material-ui/core/TextField"
+import React, { useEffect, useRef, useState } from "react"
+import io from "socket.io-client"
 import styled from 'styled-components'
+import '../../styles/MultiplayerPage.css'
 
 const Button = styled.button`
   background: #3D3883;
@@ -16,16 +19,82 @@ const Button = styled.button`
   }
 `
 
-function findMatch(){
-  console.log("Find match clicked");
-}
-
 const Multiplayer = () => {
+
+  function findMatch() {
+    console.log("Find match clicked");
+  }
+
+  const [state, setState] = useState({ message: "", name: "" })
+  const [chat, setChat] = useState([])
+
+  const socketRef = useRef()
+
+  useEffect(
+    () => {
+      socketRef.current = io.connect("151.197.236.81:4000")
+      socketRef.current.on("message", ({ name, message }) => {
+        setChat([...chat, { name, message }])
+      })
+      return () => socketRef.current.disconnect()
+    },
+    [chat]
+  )
+
+  const onTextChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const onMessageSubmit = (e) => {
+    const { name, message } = state
+    socketRef.current.emit("message", { name, message })
+    e.preventDefault()
+    setState({ message: "", name })
+  }
+
+  const renderChat = () => {
+    return chat.map(({ name, message }, index) => (
+      <div key={index}>
+        <h3>
+          {name}: <span>{message}</span>
+        </h3>
+      </div>
+    ))
+  }
+
+
   return (
-    <div className='multiplayer-base'>
-      <div>Multiplayer</div>
+    <div>
+      <div className='multiplayer-base'>
+        <div>Multiplayer</div>
         <Button onClick={findMatch} >Find Match</Button>
       </div>
+
+      <div className="card">
+        <form onSubmit={onMessageSubmit}>
+          <h1>Messenger</h1>
+          <div className="name-field">
+            <TextField name="name" onChange={(e) => onTextChange(e)} value={state.name} label="Name" />
+          </div>
+          <div>
+            <TextField
+              name="message"
+              onChange={(e) => onTextChange(e)}
+              value={state.message}
+              id="outlined-multiline-static"
+              variant="outlined"
+              label="Message"
+            />
+          </div>
+          <button>Send Message</button>
+        </form>
+        <div className="render-chat">
+          <h1>Chat Log</h1>
+          {renderChat()}
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
