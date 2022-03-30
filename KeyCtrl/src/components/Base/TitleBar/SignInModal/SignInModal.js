501a50daeffd2c09dc90e8769bf864199c57f5e3
ColinHarker <colinharker5@yahoo.com>
2022-03-30 15:56:26 -0400

    Update SignInModal.js

--- KeyCtrl/src/components/Base/TitleBar/SignInModal/SignInModal.js
+++ KeyCtrl/src/components/Base/TitleBar/SignInModal/SignInModal.js
@@ -61,7 +61,7 @@ const SignInModal = ({ accountInfo, setLoading, loggedIn, onLogin, showSignIn, s
 
         var account = await api.callLogin(hash.toString(), photo, name)
 
-        if (account.account_id === null) {
+        if (account.account_id === -1) {
             var name = name.substring(0, 14)
             var socialId = Math.floor(Math.random() * (9999 - 1000) + 1000)
             var noSpaceName = name.replace(/\s+/g, '')
